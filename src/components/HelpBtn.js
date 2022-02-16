1350dc6a86bc91186fba8c2ed5052363f3e26d0c
Ashutosh Kumar Singh <71124485+ashutosh44ks@users.noreply.github.com>
2022-02-16 20:31:31 +0530

    add more responsiveness

--- src/components/HelpBtn.js
+++ src/components/HelpBtn.js
@@ -20,7 +20,7 @@ const HelpBtn = () => {
         Help
       </Button>
       <Modal open={open} onClose={handleClose}>
-        <Box sx={style}>
+        <Box className="modal">
           <Typography variant="h5" sx={{ textAlign: "center", mb: "1em" }}>
             Rules and Info
           </Typography>
@@ -39,15 +39,6 @@ const HelpBtn = () => {
   );
 };
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: "30vw",
-  bgcolor: "background.paper",
-  boxShadow: 24,
-  p: 4,
-};
+
 
 export default HelpBtn;
